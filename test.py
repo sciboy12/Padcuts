c2c04689d2fa809cf14d0fa5a4abc7e7c5acbdc5
sciboy12 <48741660+sciboy12@users.noreply.github.com>
2019-06-18 11:32:17 -0400

    Add WIP osu!Taiko support
    This functionality will be migrated to a separate project once properly working.

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# Example code
 import padcuts as pc
 from time import sleep
 from evdev import ecodes, events, UInput, InputDevice, list_devices
@@ -5,17 +6,31 @@ import numpy as np
 import re
 from Xlib.display import Display
 from Xlib.ext.xtest import fake_input
+from subprocess import call
 d = Display()
 
-touchpad_x_min = 1266
-touchpad_x_max = 5676
+#touchpad_x_min = 1266
+#touchpad_x_max = 5676
 
-touchpad_y_min = 1062
-touchpad_y_max = 4690
+#touchpad_y_min = 1062
+#touchpad_y_max = 4690
 
+
+
+# Key config
+key_katsu_l = 'e'
+key_katsu_r = 'r'
+key_don_l = 'u'
+key_don_r = 'i'
+
+
+
+# Pressure threshold to trigger keypress
+threshold = 30
+
+# Grid settings
 grid_x = 4
 grid_y = 1
-
 '''
 a = 1
 if a == 1:
@@ -26,51 +41,107 @@ print(c)
  
 
 grid_x = len(pc.get_count(pc.gen_array(grid_x)))
-grid_y = len(pc.get_count(pc.gen_array(grid_y)))
-
-#rows = rows[1]
-#cols = cols[2]
-pad = pc.detect_touchpad()
-pad = InputDevice(pad)
 #print(grid_x)
-#print(grid_y)
-
-
-#print(grid_y)
-#section_count_x = grid[3]
-#section_count_y = grid[4]
-#print(section_count_rows)
-#device = pad
-#device = 'dev/input/event0"'
-
-#print(device)
-#grid_y = tg.gen_grid(touchpad_x_min, touchpad_x_max, touchpad_y_min, touchpad_y_max, cols)
-
-#n = 0
+grid_y = len(pc.get_count(pc.gen_array(grid_y)))
+pad = pc.detect_touchpad()
+capabilities = pc.detect_capabilities(pad)
+print(capabilities)
+touchpad_x_min = capabilities[0]
+touchpad_x_max = capabilities[1]
 axis = 'x'
-#section = 2
-#pad.grab()
+key1 = False
+key2 = False
+key3 = False
+key4 = False
+keydown = False
+pad.grab()
 while True:
-    value = pc.get_value(pad, 'x')
-    #print(value)
+    value = pc.get_value(pad)
+   
+
     if value != None:
-        print(value)
-        #print(value)
-        result = pc.check_position_cycle(pad, value, axis, grid_x, touchpad_x_min, touchpad_x_max)
-        #result = pc.check_position(pad, value, axis, grid_x, touchpad_x_min, touchpad_x_max)
-        print(result)
+        if value[0]  == 'x' and value[1] != None:
+            x = value[1]
+            x_set = True
+        elif not 'x_set' in locals():
+            x = 0
+            
+        if value[0]  == 'p' and value[1] != None:
+            p = value[1]
+            p_set = True
+        elif not 'p_set' in locals():
+            p = 0
 
-    #else:
-        
-    #if result != None:
+        result = pc.check_position_cycle(pad, x, p, threshold, axis, grid_x, touchpad_x_min, touchpad_x_max)
         #print(result)
-    #value = result[1]
-    #result = result[2]
-    
-    #sleep(0.05)
-    #print(value)
+        print(result)
+        #if result != None:
+            #print(result)
 
-    #print(result)
-    #n = n + 
-    #print(result)
-    #print(n)
+        # End of example code and start of TaikoPad
+
+        
+        
+        # Key down statments
+        #while result != False:
+        if keydown == False:
+            if result == 1:
+                call(['xdotool', 'key', key_katsu_l])
+                keydown = True
+                
+            elif result == 2:
+                call(['xdotool', 'key', key_katsu_r])
+                keydown = True
+
+            elif result == 3:
+                call(['xdotool', 'key', key_don_l])
+                keydown = True
+
+            elif result == 4:
+                call(['xdotool', 'key', key_don_r])
+                keydown = True
+        elif result == None:
+            keydown = False
+
+        
+        '''
+        if key1 or key2 or key3 or key4 == False:
+            if result == 1:
+                call(['xdotool', 'keydown', key_katsu_l])
+                key1 = True
+                
+            elif result == 2:
+                call(['xdotool', 'keydown', key_katsu_r])
+                key2 = True
+
+            elif result == 3:
+                call(['xdotool', 'keydown', key_don_l])
+                key3 = True
+
+            elif result == 4:
+                call(['xdotool', 'keydown', key_don_r])
+                key4 = True
+        elif key1 or key2 or key3 or key4 == True:
+
+            if result == None:
+                call(['xdotool', 'keyup', key_katsu_l])
+                key1 = False
+                
+            elif result == None:
+                call(['xdotool', 'keyup', key_katsu_r])
+                key1 = False
+
+            elif result == None:
+                call(['xdotool', 'keyup', key_don_l])
+                key1 = False
+                
+            elif result == None:
+                call(['xdotool', 'keyup', key_don_r])
+                key1 = False
+        '''
+        
+
+            
+        #elif result == None:
+         #   call(['xdotool', 'keyup',  key_katsu_l, key_katsu_r, key_don_l, key_don_r])
+          #  keydown = False
