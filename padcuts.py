e6294cc9396d15616f108253b230e7de38d6c0fb
sciboy12 <48741660+sciboy12@users.noreply.github.com>
2019-07-06 13:35:32 -0400

    Add comments, cleanup code
    Also added basic osu!Taiko functionality in test.py (glitchy, still WIP)

--- padcuts.py
+++ padcuts.py
@@ -1,69 +1,27 @@
 import padcuts as pc
-#from time import sleep, time, clock
+from time import sleep, time, clock
 from evdev import ecodes, events, UInput, InputDevice, list_devices
 import numpy as np
 interp = np.interp
 import re
 from Xlib.display import Display
 from Xlib.ext.xtest import fake_input
-#import timeit
 d = Display()
-
-'''
-def detect_touchpad():
-    devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-    
-    for device in devices:
-            #global touchpad
-            if bool(re.search('Touchpad', device.name)) == True:
-                    #print(evdev.InputDevice(device.path))
-                    touchpad = evdev.InputDevice(device.path)
-                    capabilities=device.capabilities(verbose=True)
-                    abs_info=capabilities.get(('EV_ABS', 3))
-                    abs_x=dict(abs_info[0:1])
-                    abs_y=dict(abs_info[1:2])
-                    return touchpad, abs_x, abs_y
-
-            if bool(re.search('TouchPad', device.name)) == True:
-                    #print(evdev.InputDevice(device.path))
-                    touchpad = evdev.InputDevice(device.path)
-                    #print(touchpad)
-                    capabilities=device.capabilities(verbose=True)
-                    abs_info=capabilities.get(('EV_ABS', 3))
-                    abs_x=dict(abs_info[0:1])
-                    abs_y=dict(abs_info[1:2])
-
-                    return touchpad, abs_x, abs_y
-'''
 
 def detect_touchpad():
 
     # Detect touchpad
     devices = [InputDevice(path) for path in list_devices()]
     for device in devices:
-            #print(device.name)
             if bool(re.search('Touchpad', device.name)) == True:
-                    #print(device.path)
+                    print(device.path)
                     touchpad = InputDevice(device.path)
-                    #pad = InputDevice(device)
-                    #return touchpad
-                    #print('test')
-            if bool(re.search('TouchPad', device.name)) == True:
-                    touchpad = InputDevice(device.path)
-                    #pad = InputDevice(device)
-                    #print(touchpad)
                     return touchpad
-                    
-    capabilities=device.capabilities(verbose=True)
-    abs_info=capabilities.get(('EV_ABS', 3))
-    abs_x=dict(abs_info[0:1])
-    abs_y=dict(abs_info[1:2])
-    return touchpad
-    #print(device)
-    # Set touchpad path
-    #device=touchpad
-
-
+                
+            if bool(re.search('TouchPad', device.name)) == True:
+                    print(device.path)
+                    touchpad = InputDevice(device.path)
+                    return touchpad
 
 def detect_capabilities(device):
 
@@ -83,27 +41,10 @@ def detect_capabilities(device):
     y_min=abs_y.min
     y_max=abs_y.max
     return abs_x.min, abs_x.max, abs_y.min, abs_y.max
-'''
-def detect_touchpad():
-# Detect touchpad
 
-
-    # Set touchpad path
-    device = detect_touchpad_path()
-    #print(device)
-    print(device)
-    # Detect min and max touchpad values
-    #capabilities=device.capabilities(verbose=True)
-    #abs_info=capabilities.get(('EV_ABS', 3))
-    #abs_x=dict(abs_info[0:1])
-    #abs_y=dict(abs_info[1:2])
-
-#    abs_x=abs_x.get(('ABS_X', 0))
- #   abs_y=abs_y.get(('ABS_Y', 1))
-
-
-    return device, abs_x.min, abs_x.max, abs_y.min, abs_y.max
-'''
+# Generates the grid to be used for the button array
+# Each square within the grid corresponds to a button
+# There are two 1D arrays, one for each axis
 def gen_grid(rows, cols):
     rows = gen_array(rows)
     cols = gen_array(cols)
@@ -111,48 +52,29 @@ def gen_grid(rows, cols):
     return grid
 
 def get_count(n):
+    # Get the total rows or columns for an axis
     count = len(n)
     return n
-def gen_array(n):
-    '''
-    #b = int(np.cos(10)*-5)
-    #b = rows * 3
-    a = 4
-    b = 25/100
-    c = b * 3 * 100
-    print(b)
-    print(c)
-    d = np.arange(x_min,x_max,c)
-    print(d)
-    print()
-    #a1 = np.tile(a, (7,1))
-    #print(a1)
-    #np.dstack((a1, a1.T)).reshape(-1, 2)
-    '''
 
+def gen_array(n):
+    # Generates the array for an axis
     #rows = 1
     grid = []
     for i in range(n):
         grid.append([])
+        # Extends the array downward into 2D
+        # This may not be needed depending on how the Y axis will be implemented
         #for j in range(cols):
          #   grid[i].append(0)
     return(grid)
 
-def parse_grid(value, grid, quadrant_x_min, quadrant_x_max):
-    
-    loop = True
-    #section = 0
-    
-    while loop == True:
-            
-        section = grid[0 + 1]
-        
-        #print(section)
-
+# Not used yet
 def click_mouse(button, release_delay):
+    #Press the button
     fake_input(d, X.ButtonPress, button)
     d.sync()
     sleep(release_delay)
+    #Release the button
     fake_input(d, X.ButtonRelease, button)
     d.sync()
 
@@ -160,49 +82,71 @@ def click_mouse(button, release_delay):
 #device = detect_touchpad()
 #print(device)
 
-
+# Returns aboslute X from the touchpad
+# Also returns the pressure for touch detection, multitouch coordinates and slot
+# Y axis not implemented yet as the rest of the code can't properly handle it in conjunction with the X axis yet
 def get_value(pad):
     if not 'pressure' in locals():
         pressure = 0
-        
+    # Get the most recent event
     event = pad.read_one()
     #print(event)
     if event != None:
+        
+        # Get X axis
         if event.type == ecodes.EV_ABS and event.code == ecodes.ABS_X:
             valtype = 'x'
-            value = event.value
-            return valtype, value
-        if event.type == ecodes.EV_ABS and event.code == ecodes.ABS_MT_POSITION_X:
-            valtype = 'mt_x'
-            value = event.value
-            return valtype, value
+            #value = event.value
+            return valtype, event.value
         
+        # Get multitouch X axis
+        elif event.type == ecodes.EV_ABS and event.code == ecodes.ABS_MT_POSITION_X:
+            valtype = 'mt_x'
+            #value = event.value
+            return valtype, event.value
+        
+        # Get pressure
         elif event.type == ecodes.EV_ABS and event.code == ecodes.ABS_PRESSURE:
             valtype = 'p'
-            value = event.value
-        else:
-            valtype = 'p'
-            value = None
-        return valtype, value
+            #value = event.value
+            return valtype, event.value
+
+        # Get slot
+        elif event.type == ecodes.EV_ABS and event.code == ecodes.ABS_MT_SLOT:
+            valtype = 'mt_s'
+            #slot_id = event.value
+            return valtype, event.value
+
 
 def check_position(value, section, section_count, limit_min, limit_max):
-
     if value != None:
-
+        
+        # Map the value to the total number of columns or rows
         value = interp(value,[limit_min,limit_max],[0,section_count])
-
+        
+        # Check if value is within range of section to section - 1 (which is the range corresonding to a single button)
         if section - 1 <= value <= (section):
-
+            # If so, return it as the button being pressed
             return section
 
-def check_position_cycle(pad, value, pressure, threshold, axis, section_count, limit_min, limit_max):
+# Loop over running check_position() and checking the value
+def check_position_loop(pad, value, pressure, threshold, axis, section_count, limit_min, limit_max):
+    # Placeholder to prevent error
     section = 1
+    
+    # Loop, with the number of loops being equal to section_count
     for x in range(0, section_count):
+        
+        # Map the value to section_count as the resulting value
         result = check_position(value, section, section_count, limit_min, limit_max)
+        
+        # Return result only if pressure passes the threshold
         if result != None and pressure >= threshold:
-            result = str(result) + str(result)
             return (result)
+        
+        # Otherwise, move on to check if result is within the next square
         else:
             section = section + 1
-    else:
-        return None
+            
+    #else:
+        #return None
--- test.py
+++ test.py
@@ -1,11 +1,6 @@
-# Example code
 import padcuts as pc
 from time import sleep
-from evdev import ecodes, events, UInput, InputDevice, list_devices
-import numpy as np
-import re
 from Xlib.display import Display
-from Xlib.ext.xtest import fake_input
 from subprocess import call
 d = Display()
 
@@ -14,8 +9,6 @@ d = Display()
 
 #touchpad_y_min = 1062
 #touchpad_y_max = 4690
-
-
 
 # Key config
 key_katsu_l = 'e'
@@ -45,20 +38,34 @@ grid_x = len(pc.get_count(pc.gen_array(grid_x)))
 grid_y = len(pc.get_count(pc.gen_array(grid_y)))
 pad = pc.detect_touchpad()
 capabilities = pc.detect_capabilities(pad)
-print(capabilities)
+#print(capabilities)
 touchpad_x_min = capabilities[0]
 touchpad_x_max = capabilities[1]
+
+# Axis to use
+# Note that the Y axis is not yet implemented in Padcuts
 axis = 'x'
-key1 = False
-key2 = False
-key3 = False
-key4 = False
+
+keydown1 = False
+keydown2 = False
+keydown3 = False
+keydown4 = False
+
+keydown1alt = False
+keydown2alt = False
+keydown3alt = False
+keydown4alt = False
+
 keydown = False
-pad.grab()
+
+
+#pad.grab()
 while True:
+    # Get touchpad values
     value = pc.get_value(pad)
    
-
+    # Set values to 0 if they do not exist yet
+    # This is to get the code to start without a NameError
     if value != None:
         if value[0]  == 'x' and value[1] != None:
             x = value[1]
@@ -72,38 +79,77 @@ while True:
         elif not 'p_set' in locals():
             p = 0
 
-        result = pc.check_position_cycle(pad, x, p, threshold, axis, grid_x, touchpad_x_min, touchpad_x_max)
-        #print(result)
-        print(result)
-        #if result != None:
-            #print(result)
-
-        # End of example code and start of TaikoPad
-
+        if value[0]  == 'mt_s' and value[1] != None:
+            s = value[1]
+            s_set = True
+        elif not 'mt_s_set' in locals():
+            s = 0
         
-        
+        result = pc.check_position_loop(pad, x, p, threshold, axis, grid_x, touchpad_x_min, touchpad_x_max)
+
+        # End of example code and start of Taiko code
+        # This will not work fully until multitouch is properly implemented
+
         # Key down statments
-        #while result != False:
-        if keydown == False:
-            if result == 1:
-                call(['xdotool', 'key', key_katsu_l])
-                keydown = True
-                
-            elif result == 2:
-                call(['xdotool', 'key', key_katsu_r])
-                keydown = True
+        if result == 1 and keydown1 == False and s == 0:
+            call(['xdotool', 'keydown', key_katsu_l])
+            keydown1 = True
+            
+        elif result == 2 and keydown2 == False and s == 0:
+            call(['xdotool', 'keydown', key_katsu_r])
+            keydown2 = True
 
-            elif result == 3:
-                call(['xdotool', 'key', key_don_l])
-                keydown = True
+        elif result == 3 and keydown3 == False and s == 0:
+            call(['xdotool', 'keydown', key_don_l])
+            keydown3 = True
 
-            elif result == 4:
-                call(['xdotool', 'key', key_don_r])
-                keydown = True
-        elif result == None:
-            keydown = False
+        elif result == 4 and keydown4 == False and s == 0:
+            call(['xdotool', 'keydown', key_don_r])
 
+        #############################################################
         
+        elif result == 1 and keydown2alt == False and s == 1:
+            call(['xdotool', 'keydown', key_katsu_r])
+            keydown2alt = True
+
+        elif result == 2 and keydown1alt == False and s == 1:
+            call(['xdotool', 'keydown', key_katsu_l])
+            keydown1alt = True
+
+        elif result == 3 and keydown4alt == False and s == 1:
+            call(['xdotool', 'keydown', key_don_r])
+            keydown4alt = True
+
+        elif result == 4 and keydown3alt == False and s == 1:
+            call(['xdotool', 'keydown', key_don_l])
+            keydown3alt = True
+
+        # Key up statments
+        elif result == None:
+            call(['xdotool', 'keyup', key_katsu_l])
+            call(['xdotool', 'keyup', key_katsu_r])
+            call(['xdotool', 'keyup', key_don_l])
+            call(['xdotool', 'keyup', key_don_r])
+            keydown1 = False
+            keydown2 = False
+            keydown3 = False
+            keydown4 = False
+            keydown1alt = False
+            keydown2alt = False
+            keydown3alt = False
+            keydown4alt = False
+
+        elif result == None and s == 1:
+            call(['xdotool', 'keyup', key_katsu_l])
+            call(['xdotool', 'keyup', key_katsu_r])
+            call(['xdotool', 'keyup', key_don_l])
+            call(['xdotool', 'keyup', key_don_r])
+            keydown1alt = False
+            keydown2alt = False
+            keydown3alt = False
+            keydown4alt = False
+
+        # Old code, kept for reference
         '''
         if key1 or key2 or key3 or key4 == False:
             if result == 1:
@@ -139,9 +185,7 @@ while True:
                 call(['xdotool', 'keyup', key_don_r])
                 key1 = False
         '''
-        
-
-            
-        #elif result == None:
-         #   call(['xdotool', 'keyup',  key_katsu_l, key_katsu_r, key_don_l, key_don_r])
-          #  keydown = False
+            # More old code
+            #elif result == None:
+                #call(['xdotool', 'keyup',  key_katsu_l, key_katsu_r, key_don_l, key_don_r])
+                #keydown = False
